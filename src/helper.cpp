30ecc00d9a3d2e59fb430afc4972d7e6acea1202
Markus Hollstein <mhollstein1@gmx.de>
2019-02-11 13:22:18 +0100

    final version

---
+++ src/helper.cpp
@@ -0,0 +1,400 @@
+
+#include <vector>
+#include <math.h>
+#include <fstream>
+#include <uWS/uWS.h>
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+// for convenience
+using namespace std;
+using json = nlohmann::json;
+
+// For converting back and forth between radians and degrees.
+constexpr double pi() { return M_PI; }
+double deg2rad(double x) { return x * pi() / 180; }
+double rad2deg(double x) { return x * 180 / pi(); }
+
+// Checks if the SocketIO event has JSON data.
+// If there is data the JSON object in string format will be returned,
+// else the empty string "" will be returned.
+string hasData(string s) {
+  auto found_null = s.find("null");
+  auto b1 = s.find_first_of("[");
+  auto b2 = s.find_first_of("}");
+  if (found_null != string::npos) {
+    return "";
+  } else if (b1 != string::npos && b2 != string::npos) {
+    return s.substr(b1, b2 - b1 + 2);
+  }
+  return "";
+}
+
+double distance(double x1, double y1, double x2, double y2)
+{
+	return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+}
+int ClosestWaypoint(double x, double y, const vector<double> &maps_x, const vector<double> &maps_y)
+{
+
+	double closestLen = 100000; //large number
+	int closestWaypoint = 0;
+
+	for(int i = 0; i < maps_x.size(); i++)
+	{
+		double map_x = maps_x[i];
+		double map_y = maps_y[i];
+		double dist = distance(x,y,map_x,map_y);
+		if(dist < closestLen)
+		{
+			closestLen = dist;
+			closestWaypoint = i;
+		}
+
+	}
+
+	return closestWaypoint;
+
+}
+
+int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
+{
+
+	int closestWaypoint = ClosestWaypoint(x,y,maps_x,maps_y);
+
+	double map_x = maps_x[closestWaypoint];
+	double map_y = maps_y[closestWaypoint];
+
+	double heading = atan2((map_y-y),(map_x-x));
+
+	double angle = fabs(theta-heading);
+  angle = min(2*pi() - angle, angle);
+
+  if(angle > pi()/4)
+  {
+    closestWaypoint++;
+  if (closestWaypoint == maps_x.size())
+  {
+    closestWaypoint = 0;
+  }
+  }
+
+  return closestWaypoint;
+}
+
+// Transform from Cartesian x,y coordinates to Frenet s,d coordinates
+vector<double> getFrenet(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
+{
+	int next_wp = NextWaypoint(x,y, theta, maps_x,maps_y);
+
+	int prev_wp;
+	prev_wp = next_wp-1;
+	if(next_wp == 0)
+	{
+		prev_wp  = maps_x.size()-1;
+	}
+
+	double n_x = maps_x[next_wp]-maps_x[prev_wp];
+	double n_y = maps_y[next_wp]-maps_y[prev_wp];
+	double x_x = x - maps_x[prev_wp];
+	double x_y = y - maps_y[prev_wp];
+
+	// find the projection of x onto n
+	double proj_norm = (x_x*n_x+x_y*n_y)/(n_x*n_x+n_y*n_y);
+	double proj_x = proj_norm*n_x;
+	double proj_y = proj_norm*n_y;
+
+	double frenet_d = distance(x_x,x_y,proj_x,proj_y);
+
+	//see if d value is positive or negative by comparing it to a center point
+
+	double center_x = 1000-maps_x[prev_wp]; //why 1000 and 2000? probably (1000,2000) is center of the map
+	double center_y = 2000-maps_y[prev_wp];
+	double centerToPos = distance(center_x,center_y,x_x,x_y);
+	double centerToRef = distance(center_x,center_y,proj_x,proj_y);
+
+	if(centerToPos <= centerToRef)
+	{
+		frenet_d *= -1;
+	}
+
+	// calculate s value
+	double frenet_s = 0;
+	for(int i = 0; i < prev_wp; i++)
+	{
+		frenet_s += distance(maps_x[i],maps_y[i],maps_x[i+1],maps_y[i+1]);
+	}
+
+	frenet_s += distance(0,0,proj_x,proj_y);
+
+	return {frenet_s,frenet_d};
+
+}
+
+// Transform from Frenet s,d coordinates to Cartesian x,y
+vector<double> getXY(double s, double d, const vector<double> &maps_s, const vector<double> &maps_x, const vector<double> &maps_y)
+{
+	int prev_wp = -1;
+
+	while(s > maps_s[prev_wp+1] && (prev_wp < (int)(maps_s.size()-1) ))
+	{
+		prev_wp++;
+	}
+
+	int wp2 = (prev_wp+1)%maps_x.size();
+
+	double heading = atan2((maps_y[wp2]-maps_y[prev_wp]),(maps_x[wp2]-maps_x[prev_wp]));
+	// the x,y,s along the segment
+	double seg_s = (s-maps_s[prev_wp]);
+
+	double seg_x = maps_x[prev_wp]+seg_s*cos(heading);
+	double seg_y = maps_y[prev_wp]+seg_s*sin(heading);
+
+	double perp_heading = heading-pi()/2;
+
+	double x = seg_x + d*cos(perp_heading);
+	double y = seg_y + d*sin(perp_heading);
+
+	return {x,y};
+
+}
+vector<double> transf_to_car_coord(double x,double y,double car_x,double car_y,double car_theta)
+{
+  //transforms global coordiates (x,y) to local car coordinates with the car being at position (0,0) and heading in x-direction
+  //returns a vector of doubles containing the x-coordinate as first and the y-coordinate as second element
+  vector<double> new_coord;
+  double new_x,new_y;
+  new_x = (x-car_x)*cos(car_theta) +(y-car_y)*sin(car_theta);
+  new_y = -(x-car_x)*sin(car_theta) +(y-car_y)*cos(car_theta);
+  new_coord.push_back(new_x);
+  new_coord.push_back(new_y);
+  return new_coord;
+};
+vector<double> transf_to_global_coord(double x,double y,double car_x,double car_y,double car_theta)
+{
+  //reverses transf_to_car_coordinates
+  //transforms local car coordinates back to global coordinates
+  //returns a vector containing transformed x and y coordinates
+  vector<double> new_coord;
+  double new_x,new_y;
+  new_x = x*cos(-car_theta) +y*sin(-car_theta)+car_x;
+  new_y = -x*sin(-car_theta) +y*cos(-car_theta)+car_y; 
+  new_coord.push_back(new_x);
+  new_coord.push_back(new_y);
+  return new_coord;
+};
+
+void shifting_lane(int goal_lane,double curr_d, double curr_s,double &end_s ,double curr_velo,vector<double> &x_pts,vector<double> &y_pts,vector<double> map_waypoints_s, vector<double> map_waypoints_x, vector<double> map_waypoints_y)
+{
+  //function to complete a lane_shift that has already begun
+  double goal_d = goal_lane*4 -2;
+  double remaining_d = abs(goal_d-curr_d);
+  double d_vel = 4./2.5; // calculate velocity in d direction for the lane shift: 4m/2.5s
+  double remaining_time = remaining_d/d_vel; //calculate the time to complete lane shift
+  double remaining_way = remaining_time * curr_velo;// way to complete
+  end_s = curr_s+remaining_way; //upadate global variable containing the s-value at the end of the lane shift
+  vector<double> xy; 
+  xy = getXY(end_s+10,goal_lane*4-2 ,map_waypoints_s,map_waypoints_x,map_waypoints_y);// some way beyond the endpoint to prevent high jerk
+  x_pts.push_back(xy[0]);
+  y_pts.push_back(xy[1]);
+  
+  xy = getXY(end_s+30, goal_lane*4-2,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+  x_pts.push_back(xy[0]);
+  y_pts.push_back(xy[1]);
+  xy = getXY(end_s+60, goal_lane*4-2,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+  x_pts.push_back(xy[0]);
+  y_pts.push_back(xy[1]);
+};
+
+vector <vector <double>> other_cars(vector <vector <double>> sensor_fusion)
+{
+  //returns a vector containing for each car a vector with its current lane, s and velocity
+  vector <vector<double>> ret;
+  for(int i=0;i<sensor_fusion.size();i++)
+  {
+    double d = sensor_fusion[i][6];
+    double lane;
+    if(0<d && d<4)
+    {
+      lane = 1;
+    }
+    else if(4<=d&& d<8)
+    {
+      lane = 2;
+    }
+    else if(8<d && d<= 12)
+    {
+      lane = 3;
+    }     
+    double s = sensor_fusion[i][5];
+    //calculate velocity of the car
+    double vel = sqrt(sensor_fusion[i][3]*sensor_fusion[i][3]+ sensor_fusion[i][4]*sensor_fusion[i][4]);
+    vector<double> car;
+    car.push_back(lane);
+    car.push_back(s);
+    car.push_back(vel);
+    ret.push_back(car);
+  }
+  return ret;
+};
+bool slow_car_ahead(vector <vector <double>> other_cars,double curr_s,int curr_lane,double max_vel)
+{
+  //checks wheter there is a slower car in the same lane and close ahead
+  //thus helps to determine when a lane shift should be considered
+  bool car_ahead=false;
+  for(int i=0;i<other_cars.size();i++)
+  {    
+    if(other_cars[i][0]==curr_lane && 0<(other_cars[i][1]-curr_s) && (other_cars[i][1]-curr_s)<60 && other_cars[i][2]< max_vel)
+    {
+      car_ahead = true;
+    }
+  }
+  return car_ahead;
+};
+double lane_speed(vector <vector <double>> other_cars,double curr_s,int lane,double max_vel)
+{
+  //calculates the speed of the car that is closest ahead in the respective lane
+  double lane_speed;
+  bool car_in_lane=false;
+  double min_s=1000; //some high value  
+  for(int i=0;i<other_cars.size();i++)
+  {    
+    if(other_cars[i][0]==lane && 0<(other_cars[i][1]-curr_s) && (other_cars[i][1]-curr_s)<50 && (other_cars[i][1]-curr_s)<min_s && other_cars[i][2]< max_vel ) // check wheter there is a car in the same lane, less than 50m away, slow and in case there are more than one only consider the closest to be able to adapt to its speed
+    {
+      lane_speed = other_cars[i][2];
+      min_s = other_cars[i][1]-curr_s;
+      car_in_lane = true;
+    }
+  }
+  if(!car_in_lane)
+  {
+    lane_speed = max_vel; // if no car is detected maximal speed is possible
+  }
+  return lane_speed;
+};
+
+
+bool laneshift_possible(int curr_lane, int shifted_lane, vector <vector <double>> other_cars, double curr_s,double curr_vel)
+{
+  // checks wheter shifted lane is neighbouring and free
+  if( abs(curr_lane - shifted_lane)!=1)
+  {    
+    return false;
+  }
+  for(int i=0;i<other_cars.size();i++)
+  {    
+    if(other_cars[i][0]==shifted_lane && -5<(other_cars[i][1]-curr_s) && (other_cars[i][1]-curr_s)<40)
+    {
+      return false;      
+    }
+    if(other_cars[i][0]==shifted_lane && -50<(other_cars[i][1]-curr_s) && (other_cars[i][1]-curr_s)<=-5 &&((curr_vel-other_cars[i][2])*2.5 - 10 -other_cars[i][1]+curr_s)<0)
+    {
+      return false;     
+    }
+  }
+  return true;
+};
+       
+bool laneshift_beneficial(int shifted_lane, vector <vector <double>> other_cars, double speed_curr_lane, double curr_s)
+{
+  //checks wheter it is beneficial to change lanes
+  bool ret=true;
+  for(int i=0;i<other_cars.size();i++)
+  {    
+    if(other_cars[i][0]==shifted_lane && 0<(other_cars[i][1]-curr_s) && (other_cars[i][1]-curr_s)<20 && other_cars[i][2]<= speed_curr_lane )
+    {
+      ret=false;      
+    }
+  }
+  return ret;
+};
+
+bool try_lane_shift(int & curr_lane,int goal,vector <vector <double>> other_cars, double speed_curr_lane, bool &lane_shift, int & goal_lane, double curr_s, double curr_vel)
+{
+  // initiates a lane shift if possible and beneficial
+  bool ret = false;
+  if(laneshift_possible(curr_lane,goal,other_cars, curr_s,curr_vel) && laneshift_beneficial(goal_lane,other_cars,speed_curr_lane,curr_s))
+  {
+    lane_shift =true;
+    goal_lane = goal;
+    ret = true;
+    curr_lane = goal_lane;
+   
+  }  
+  return ret;
+};
+
+vector <double> lane_values(vector <vector <double>> other_cars, double curr_s)
+{
+  //determine values of the lanes where lanes with more free space ahead are better
+  vector <double> values{1000,1000,1000};
+  for(int i=1;i<4;i++)
+  {
+    double curr_value, dist, speed;
+    for(int j=0;j<other_cars.size();j++)
+    {
+      if( -5 >(other_cars[i][1]-curr_s) || (other_cars[i][1]-curr_s)>300)
+      {
+        curr_value =1000;
+      } 
+      else if(  (other_cars[i][1]-curr_s)>=10)
+      {
+        curr_value = (other_cars[i][1]-curr_s);//+10*other_cars[i][2];
+      }
+      else if(-5<(other_cars[i][1]-curr_s)  && other_cars[i][1]-curr_s <10)
+      {
+        curr_value = 0;
+      }
+      if(other_cars[j][0] ==i && (curr_value < values[i-1]))
+      {
+        values[i-1] = curr_value;
+      }
+    }
+  }
+  return values;
+};
+vector <int> classify_lanes(vector <double> values)
+{  
+  // order the lanes based on its values and return a list of integers where the first element is the number of the best lane and last element of the list contains the worst lane
+  vector <int> ret_vec{2,1,3};
+  if(values[0]>values[1] && values[0]>=values[2] && values[1] >= values[2])
+  {
+    ret_vec[0]=1;
+    ret_vec[1]=2;
+    ret_vec[2]=3;
+  }
+  else if(values[0]>values[1] && values[0]>=values[2] && values[1] < values[2])
+  {
+    ret_vec[0]=1;
+    ret_vec[1]=3;
+    ret_vec[2]=2;
+  }
+  else if(values[2]>values[1] && values[2]>values[0] && values[1] < values[0])
+  {
+    ret_vec[0]=3;
+    ret_vec[1]=1;
+    ret_vec[2]=2;    
+  }
+  else if(values[2]>values[1] && values[2]>values[0] && values[1] >= values[0])
+  {
+    ret_vec[0]=3;
+    ret_vec[1]=2;
+    ret_vec[2]=1;
+  }
+  else if(values[1]>=values[0] && values[1]>=values[2] && values[0] < values[2])
+  {
+    ret_vec[0]=2;
+    ret_vec[1]=3;
+    ret_vec[2]=1;
+  }
+  else //if(values[1]>=values[0] && values[1]>=values[2] && values[0] >= values[2])
+  {
+    ret_vec[0]=2;
+    ret_vec[1]=1;
+    ret_vec[2]=3; 
+  }
+  return ret_vec;
+};
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+
 #include <fstream>
 #include <math.h>
 #include <uWS/uWS.h>
@@ -8,160 +9,14 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
+#include "helper.cpp"
+
 
 using namespace std;
 
-// for convenience
-using json = nlohmann::json;
 
-// For converting back and forth between radians and degrees.
-constexpr double pi() { return M_PI; }
-double deg2rad(double x) { return x * pi() / 180; }
-double rad2deg(double x) { return x * 180 / pi(); }
 
-// Checks if the SocketIO event has JSON data.
-// If there is data the JSON object in string format will be returned,
-// else the empty string "" will be returned.
-string hasData(string s) {
-  auto found_null = s.find("null");
-  auto b1 = s.find_first_of("[");
-  auto b2 = s.find_first_of("}");
-  if (found_null != string::npos) {
-    return "";
-  } else if (b1 != string::npos && b2 != string::npos) {
-    return s.substr(b1, b2 - b1 + 2);
-  }
-  return "";
-}
-
-double distance(double x1, double y1, double x2, double y2)
-{
-	return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-}
-int ClosestWaypoint(double x, double y, const vector<double> &maps_x, const vector<double> &maps_y)
-{
-
-	double closestLen = 100000; //large number
-	int closestWaypoint = 0;
-
-	for(int i = 0; i < maps_x.size(); i++)
-	{
-		double map_x = maps_x[i];
-		double map_y = maps_y[i];
-		double dist = distance(x,y,map_x,map_y);
-		if(dist < closestLen)
-		{
-			closestLen = dist;
-			closestWaypoint = i;
-		}
-
-	}
-
-	return closestWaypoint;
-
-}
-
-int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
-{
-
-	int closestWaypoint = ClosestWaypoint(x,y,maps_x,maps_y);
-
-	double map_x = maps_x[closestWaypoint];
-	double map_y = maps_y[closestWaypoint];
-
-	double heading = atan2((map_y-y),(map_x-x));
-
-	double angle = fabs(theta-heading);
-  angle = min(2*pi() - angle, angle);
-
-  if(angle > pi()/4)
-  {
-    closestWaypoint++;
-  if (closestWaypoint == maps_x.size())
-  {
-    closestWaypoint = 0;
-  }
-  }
-
-  return closestWaypoint;
-}
-
-// Transform from Cartesian x,y coordinates to Frenet s,d coordinates
-vector<double> getFrenet(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
-{
-	int next_wp = NextWaypoint(x,y, theta, maps_x,maps_y);
-
-	int prev_wp;
-	prev_wp = next_wp-1;
-	if(next_wp == 0)
-	{
-		prev_wp  = maps_x.size()-1;
-	}
-
-	double n_x = maps_x[next_wp]-maps_x[prev_wp];
-	double n_y = maps_y[next_wp]-maps_y[prev_wp];
-	double x_x = x - maps_x[prev_wp];
-	double x_y = y - maps_y[prev_wp];
-
-	// find the projection of x onto n
-	double proj_norm = (x_x*n_x+x_y*n_y)/(n_x*n_x+n_y*n_y);
-	double proj_x = proj_norm*n_x;
-	double proj_y = proj_norm*n_y;
-
-	double frenet_d = distance(x_x,x_y,proj_x,proj_y);
-
-	//see if d value is positive or negative by comparing it to a center point
-
-	double center_x = 1000-maps_x[prev_wp];
-	double center_y = 2000-maps_y[prev_wp];
-	double centerToPos = distance(center_x,center_y,x_x,x_y);
-	double centerToRef = distance(center_x,center_y,proj_x,proj_y);
-
-	if(centerToPos <= centerToRef)
-	{
-		frenet_d *= -1;
-	}
-
-	// calculate s value
-	double frenet_s = 0;
-	for(int i = 0; i < prev_wp; i++)
-	{
-		frenet_s += distance(maps_x[i],maps_y[i],maps_x[i+1],maps_y[i+1]);
-	}
-
-	frenet_s += distance(0,0,proj_x,proj_y);
-
-	return {frenet_s,frenet_d};
-
-}
-
-// Transform from Frenet s,d coordinates to Cartesian x,y
-vector<double> getXY(double s, double d, const vector<double> &maps_s, const vector<double> &maps_x, const vector<double> &maps_y)
-{
-	int prev_wp = -1;
-
-	while(s > maps_s[prev_wp+1] && (prev_wp < (int)(maps_s.size()-1) ))
-	{
-		prev_wp++;
-	}
-
-	int wp2 = (prev_wp+1)%maps_x.size();
-
-	double heading = atan2((maps_y[wp2]-maps_y[prev_wp]),(maps_x[wp2]-maps_x[prev_wp]));
-	// the x,y,s along the segment
-	double seg_s = (s-maps_s[prev_wp]);
-
-	double seg_x = maps_x[prev_wp]+seg_s*cos(heading);
-	double seg_y = maps_y[prev_wp]+seg_s*sin(heading);
-
-	double perp_heading = heading-pi()/2;
-
-	double x = seg_x + d*cos(perp_heading);
-	double y = seg_y + d*sin(perp_heading);
-
-	return {x,y};
-
-}
 
 int main() {
   uWS::Hub h;
@@ -199,14 +54,18 @@ int main() {
   	map_waypoints_dx.push_back(d_x);
   	map_waypoints_dy.push_back(d_y);
   }
-
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  bool lane_shift=false; //define a variable to save the information wheter a lane_shift is going on and one to save the goal lane
+  int goal_lane = 0;
+  double end_shift_s;//save the s value of the end of a lane shift to determine wheter it is completed
+  int curr_lane = 2;
+  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&end_shift_s,&goal_lane, & lane_shift, & curr_lane](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
     //auto sdata = string(data).substr(0, length);
     //cout << sdata << endl;
+
     if (length && length > 2 && data[0] == '4' && data[1] == '2') {
 
       auto s = hasData(data);
@@ -241,12 +100,162 @@ int main() {
 
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
+          
+          	//create points to make a spline 
+          	vector<double> x_pts;
+          	vector<double> y_pts;
+          	double prev_x;
+          	double prev_y;
+          	// create variable to calculate the speed at the end of the previous path (if existing; else use car_speed)
+          	double  curr_speed;
+          	if(previous_path_x.size()<2) // if not enough points from the previous path are remaining use an artificial previous point and the current position to create a smooth path:
+          	{
+          		//some artificial previous point in order to make a smooth transition
+          		prev_x = car_x-cos(car_yaw);
+          		prev_y = car_y-sin(car_yaw);
+          		x_pts.push_back(prev_x);
+          		y_pts.push_back(prev_y);
+          		//current car coordinates
+          		x_pts.push_back(car_x);
+          		y_pts.push_back(car_y);
+              	end_path_s = car_s;// if there is no previous path set the end of the previous path to the car position
+              	curr_speed = car_speed;
+            }
+          	else// if there are points from the previous path in my list use the last two to ensure a smooth path
+            { 
+              	prev_x = previous_path_x[previous_path_x.size()-2];
+              	prev_y = previous_path_y[previous_path_y.size()-2];
+              	x_pts.push_back(prev_x);
+              	x_pts.push_back(previous_path_x[previous_path_x.size()-1]);
+              	y_pts.push_back(prev_y);
+              	y_pts.push_back(previous_path_y[previous_path_y.size()-1]);
+              	double  old_dist;
+          		//calculate speed at the end of the previous path by calculating the distance between the last two points and dividing by 0.02
+          		old_dist = distance(previous_path_x[previous_path_x.size()-1],previous_path_y[previous_path_y.size()-1],previous_path_x[previous_path_x.size()-2],previous_path_y[previous_path_y.size()-2]);
+          		curr_speed = old_dist/0.02;
+            }
+          	if(lane_shift) // check wheter lane shift is going on
+            {
+              	if(end_path_s > end_shift_s)//check wheter lane shift was completed within the already calculated path
+                {
+                  lane_shift = false;
+                  curr_lane = goal_lane;
+                  // lane shift finished; then create 3 further points in the current lane for calculating the spline
+                  vector<double> xy;
+          		  xy = getXY(end_path_s+30, curr_lane*4-2,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+          		  x_pts.push_back(xy[0]);
+          		  y_pts.push_back(xy[1]);
+          		  xy = getXY(end_path_s+60, curr_lane*4-2,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+          		  x_pts.push_back(xy[0]);
+          		  y_pts.push_back(xy[1]);
+          		  xy = getXY(end_path_s+90, curr_lane*4-2,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+          		  x_pts.push_back(xy[0]);
+          		  y_pts.push_back(xy[1]);
+                }
+              	else 
+                { 
+                  	//for an ongoing laneshift calculate more points for the spline
+              		shifting_lane(goal_lane,end_path_d, end_path_s,end_shift_s ,curr_speed,x_pts,y_pts,map_waypoints_s, map_waypoints_x, map_waypoints_y);
+                }
+            }          	
+            else//no laneshift so just create points in the same lane for the spline
+            {
+          		vector<double> xy;
+          		xy = getXY(end_path_s+30, curr_lane*4-2,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+          		x_pts.push_back(xy[0]);
+          		y_pts.push_back(xy[1]);
+          		xy = getXY(end_path_s+60, curr_lane*4-2,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+          		x_pts.push_back(xy[0]);
+          		y_pts.push_back(xy[1]);
+          		xy = getXY(end_path_s+90, curr_lane*4-2,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+          		x_pts.push_back(xy[0]);
+          		y_pts.push_back(xy[1]);              	
+            }
+          	//transform x_pts and y_pts to local car coordinates where it (hopefully) defines a function (else same x values are possible; for example (assuming a circle) at 3 and 9 o'clock)
+          	vector<double> x_trans;
+          	vector<double> y_trans;
+          	double yaw = atan2(y_pts[1]-y_pts[0],x_pts[1]-x_pts[0]);//necessary for transforming between global and local car coordinates
+          	          	
+          	for(int i = 0 ;i<x_pts.size();i++)
+            {
+              vector<double> transf;              
+              transf = transf_to_car_coord(x_pts[i],y_pts[i],x_pts[1],y_pts[1],yaw);
+              x_trans.push_back(transf[0]);
+              y_trans.push_back(transf[1]);              
+            }
+          	next_x_vals.insert(next_x_vals.begin(),previous_path_x.begin(),previous_path_x.end());
+          	next_y_vals.insert(next_y_vals.begin(),previous_path_y.begin(),previous_path_y.end());
+          	
+          	tk::spline s; //with the transformed coordinates create a spline fitting the above created points
+   			s.set_points(x_trans,y_trans);
 
+          	//now create a trajectory completing the previous path and using the above created spline
+          	double target_x = 30.; //length of the trajectory in x-direction
+          	double target_y= s(target_x); //y-value of the endpoint (in car coordinates)
+          	double target_dist = distance(0,0,target_x,target_y); //total distance
+          	double x_add =0.;
+          	double max_accel = 8.5;// acceleration is supposed to remain under 10 m/s^2
+          	
+          	double max_speed = 49./2.25;//set maximal speed to 49 mph and transform to m/s
+           	double target_speed= max_speed; //define a speed the car is supposed to drive; if possible this will be the maximal speed, if there is slow traffic ahead it will be adjusted below
+			double curr_accel;
+          	vector <vector <double>> cars = other_cars(sensor_fusion); //calculate lane, s-value and speed of all other cars
+          	bool traffic = slow_car_ahead(cars,car_s,curr_lane,max_speed); //check wheter there is another car close ahead
+          	double lane_spe = lane_speed(cars,car_s, curr_lane, max_speed); //calculate a maximal speed that is save without risking to  crash into a car ahead
+          	
+            bool shift = false;
+          	if(traffic && (!lane_shift))
+            {              
+              vector <double>  values = lane_values(cars,car_s); //calculate values of the lanes to determine which is best
+              vector <int> classification  = classify_lanes( values);
 
-          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+              // try shifting into the best lan               
+              shift = try_lane_shift(curr_lane,classification[0],cars,lane_spe,lane_shift,goal_lane,car_s,car_speed);                
+              
+              if( (!shift) )
+              {                
+                // if the best lane was not possible try shifting to the second best
+                shift = try_lane_shift(curr_lane,classification[1],cars,lane_spe,lane_shift,goal_lane,car_s,car_speed);                
+              }
+              if(!shift)
+              {
+                // if it was not reasonable to shift lanes adjust your target speed to the car ahead
+                target_speed = lane_spe;
+              }
+            }
+          	          	
+    		for(int i = previous_path_x.size(); i < 100; i++) //fill up the previous path to get 100 points
+    		{
+              	// check if the current speed is close to the maximal speed and adjust acceleration accordingly
+              	if(fabs(curr_speed-target_speed)<.05)
+                {
+                  curr_accel = 0;
+                }
+              	else if(curr_speed<target_speed)
+                {
+                  curr_accel = max_accel;
+                }
+              	else if(curr_speed>target_speed)
+                {
+                  curr_accel = - max_accel;
+                }
+              	
+              	curr_speed += curr_accel*.02;// current speed (at the end of the previous path) was calculated above; update it here using the current acceleration and the time 0.02 between two steps
+              	double N = target_dist/(.02*curr_speed);//calculate how many steps it would take with the current distance to complete the target distance
+              	double x_point = x_add + target_x/N;// next x-point 
+              	double y_point = s(x_point);//next y-point
+              
+              	x_add = x_point;
+              	vector<double> retrans;
+              	retrans = transf_to_global_coord(x_point,y_point,x_pts[1],y_pts[1],yaw);//retransform the new point to global coordinates 
+              	
+              	next_x_vals.push_back(retrans[0]);
+              	next_y_vals.push_back(retrans[1]);                         
+
+    		}
+
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
-
           	auto msg = "42[\"control\","+ msgJson.dump()+"]";
 
           	//this_thread::sleep_for(chrono::milliseconds(1000));
